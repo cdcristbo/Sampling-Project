0a0b2e485c49c4dd367c4c0518c154367772c6ca
carlos <ccristbo@gmail.com>
2023-02-13 14:04:11 -0500

    pdf

--- sampling.R
+++ sampling.R
@@ -1,7 +1,5 @@
 library(readxl)
 library(dplyr)
-library(tidyverse)
+#library(tidyverse)
 frame <- read_excel("MI_school_frame_head_counts.xls")
-sample <- read_excel("sample_school_student_list.xls") %>% 
-  drop_na()
-
+sample <- read_excel("sample_school_student_list.xls")
